ca66466bc38563072c27f121ffbd61615fc8e908
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-01-22 01:02:46 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_aciton :authenticate_user!
+  before_action :configure_permitted_parameters, if: devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in,keys: [:name])
 end
