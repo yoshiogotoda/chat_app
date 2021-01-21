f3d25caea0382c8c1d0436ca378e1d6bafe4b16c
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-01-22 08:23:04 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_aciton :authenticate_user!
-  before_action :configure_permitted_parameters, if: devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in,keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+  end
 end
