cc860ccb3cd80de06adbcf3f68b7a11109ac2b92
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-01-22 23:54:28 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :tweets 
+  resources :messages, only: :index
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
