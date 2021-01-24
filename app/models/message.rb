521440a2cab949839c190dbe512a82d7aca49d28
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-01-24 10:55:56 +0900

    Revert "Revert "各モデルのテスト""
    This reverts commit 6135e06058cd14b4aedfa95226fc20fe4c60b5b2.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,11 @@
 class Message < ApplicationRecord
-  belongs_to :user
   belongs_to :room
+  belongs_to :user
   has_one_attached :image
 
-  validates :content,presence: true, unless: was_attached?
+  validates :content, presence: true, unless: :was_attached?
 
   def was_attached?
-   self.image.attached?
+    self.image.attached?
   end
 end
