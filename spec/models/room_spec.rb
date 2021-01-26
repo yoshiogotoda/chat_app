4996242c3d279f93a622840e856bb242f48f19d4
yoshitogotoda <yoshitogotoda@yahoo.co.jp>
2021-01-26 22:36:32 +0900

    room単体コード作成

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it "nameの値が存在すれば登録できること" do
+      expect(@room).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @room.name = nil
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
